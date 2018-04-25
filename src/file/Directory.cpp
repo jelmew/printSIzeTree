f332d9912cb922919324c17f7a3c7289b37838d9
Jelmer van amen <jelmer.van.amen@crv4all.com>
2018-04-25 09:23:18 +0200

    Fix initalization error

--- src/file/Directory.cpp
+++ src/file/Directory.cpp
@@ -7,6 +7,7 @@
 using std::string;
 
 Directory::Directory(FormatterInterface *formatterInterface) {
+    size_on_disk=0;
     this->formatterInterface = formatterInterface;
     _path = fs::current_path();
     for (fs::directory_entry p: fs::directory_iterator(_path)) {
@@ -23,16 +24,7 @@ Directory::Directory(FormatterInterface *formatterInterface) {
 }
 
 file_size_in_bytes Directory::get_size() const {
-    file_size_in_bytes size = 0;
-    for (const auto &file1: _files) {
-        size = size + file1.size();
-    }
-    for (const auto &directory: directories) {
-        size += 4000;
-        size += directory.get_size();
-    }
-
-    return size;
+    return size_on_disk;
 }
 
 void Directory::print_size_tree() {
@@ -56,18 +48,23 @@ std::vector<std::pair<string, file_size_in_bytes>> Directory::get_vector() const
 
 
 Directory::Directory(boost::filesystem::path _path) {
+    this->size_on_disk=0;
     this->formatterInterface = nullptr;
     this->_path = _path;
-    for (fs::directory_entry p: fs::directory_iterator(_path)) {
-        if (fs::is_regular_file(p)) {
-            const File &file = File(p.path());
-            size_on_disk += file.size();
-            _files.push_back(file);
-        } else if (fs::is_directory(p)) {
-            const Directory &directory = Directory(p.path());
-            size_on_disk += directory.get_size();
-            directories.push_back(directory);
+    try {
+        for (fs::directory_entry p: fs::directory_iterator(_path)) {
+            if (fs::is_regular_file(p)) {
+                const File &file = File(p.path());
+                size_on_disk += file.size();
+                _files.push_back(file);
+            } else if (fs::is_directory(p)) {
+                const Directory &directory = Directory(p.path());
+                size_on_disk += directory.get_size();
+                directories.push_back(directory);
+            }
         }
+    } catch (const fs::filesystem_error &e) {
+        std::cout << "Could not list" << _path << std::endl;
     }
 }
 
